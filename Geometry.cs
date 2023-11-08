1065162932ca87ad40ed75f9cfe4e6ca1f150cc2
Giosan Stefana <giosanstefana@yahoo.com>
2023-11-08 18:07:23 +0200

    first commit

---
+++ Geometry.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab1
+{
+    public abstract class Geometry
+    {
+        public Color Color { get; set; }
+        public Material Material { get; set; }
+
+        public Geometry(Material material, Color color)
+        {
+            Material = material;
+            Color = color;
+        }
+
+        public abstract Intersection GetIntersection(Line line, double minDist, double maxDist);
+
+        public abstract Vector Normal(Vector v);
+    }
+}
